8f46faaea70c1d73e24d7e9e36c77242cfbd4e4d
Kyle Nugent <kln5656@gmail.com>
2020-07-03 19:33:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,9 @@ function viewCart() {
   }else if (cart.length = 1){
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
   }
+  else if (cart.length = 2){
+    return `In your cart, you have ${cart[1].itemName} at $${cart[1].itemPrice}.`;
+  }
 }
 
 function total() {
