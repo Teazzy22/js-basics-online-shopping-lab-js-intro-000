d6e026c3f6102f3998d75e94cc75d4c45c4ddcdb
Kyle Nugent <kln5656@gmail.com>
2020-07-03 19:31:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
-  if (cart <= 0){
-    return("Your shopping cart is empty.");
-  }else {
-    return `In your cart, you have ${itemObj.itemName} at ${itemObj.itemPrice}.`;
+  if (cart.length === 0){
+    return "Your shopping cart is empty.";
+  }else if (cart.length = 1){
+    return `In your cart, you have ${cart[0]} at ${cart[0].itemPrice}.`;
   }
 }
 
