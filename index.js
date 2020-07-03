02811f7bbe3b431e18d09f6370d8ecac7d174b6d
Kyle Nugent <kln5656@gmail.com>
2020-07-03 19:36:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,9 +23,10 @@ function viewCart() {
     return "Your shopping cart is empty.";
   }else if (cart.length === 1){
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
-  }
-  else if (cart.length === 2){
+  }else if (cart.length === 2){
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
+  }else if (cart.length === 3){
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}, and ${cart[2].itemName} at $${cart[2].itemPrice}`;
   }
 }
 
